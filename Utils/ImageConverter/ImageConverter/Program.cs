c1d55855e3cf83e14b0b9f084537b87f2b585db7
Colin Domoney <cdomoney@veracode.com>
2017-04-07 12:07:52 +0100

    Working on processing the PPM file.

--- Utils/ImageConverter/ImageConverter/Program.cs
+++ Utils/ImageConverter/ImageConverter/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
 
 namespace ImageConverter
 {
@@ -13,9 +16,36 @@ namespace ImageConverter
 
             Console.WriteLine("Line count: {0}", allLines.Length);
 
-            Console.WriteLine("Line 1 = '{0}'", allLines[0]);
+            // Check the file makes sense
+            int colsX, rowsY;
+            bool fileOK = GetFileInfo(allLines, out colsX, out rowsY);
+
+            Console.WriteLine("File OK : {0}", fileOK);
 
             Console.WriteLine("Done!");
+        }
+
+
+        private static bool GetFileInfo(string[] allLines, out int colsX, out int rowsY)
+        {
+            // First check we have at least one line
+            if (allLines.Length < 1)
+            {
+                throw new FileLoadException("File contains no data");
+            }
+
+            // Check the file starts with 'P3'
+            if (allLines[0].StartsWith("P3") == false)
+            {
+                throw new FileLoadException("File does not contain PPM identifier");
+            }
+
+            // Get the X and Y co-ords
+
+            colsX = 0;
+            rowsY = 0;
+
+            return false;
         }
     }
 }
