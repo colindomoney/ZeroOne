214ab90508a74471cd897d1b67407271d0a4c227
Colin Domoney <cdomoney@veracode.com>
2017-04-07 11:42:34 +0100

    Add the ImageConverter project.

---
+++ Utils/ImageConverter/ImageConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageConverter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ImageConverter starting ...");
+
+            string[] allLines = System.IO.File.ReadAllLines("../../../../../Graphics/01/01_38x29_asc.ppm");
+
+            Console.WriteLine("Line count: {0}", allLines.Length);
+
+            Console.WriteLine("Line 1 = '{0}'", allLines[0]);
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
