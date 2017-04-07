99d1fed2e305939134c684e5f6f967ee11cdf3e4
Colin Domoney <cdomoney@veracode.com>
2017-04-07 13:42:38 +0100

    Got the file parsing stuff working.

--- Utils/ImageConverter/ImageConverter/Program.cs
+++ Utils/ImageConverter/ImageConverter/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.IO;
+using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Text.RegularExpressions;
 
 namespace ImageConverter
 {
@@ -28,6 +30,14 @@ namespace ImageConverter
 
         private static bool GetFileInfo(string[] allLines, out int colsX, out int rowsY)
         {
+            colsX = 0;
+            rowsY = 0;
+
+            // First clean up the input data
+            allLines = allLines.Where(l => String.IsNullOrEmpty(l) == false).ToArray();
+
+            int firstLine = 1;
+
             // First check we have at least one line
             if (allLines.Length < 1)
             {
@@ -41,9 +51,30 @@ namespace ImageConverter
             }
 
             // Get the X and Y co-ords
+            while (true)
+            {
+                string line = allLines[firstLine++];
+                Regex rgx = new Regex(@"^(\d+)\s(\d+)");
+                Match mtc = rgx.Match(line);
 
-            colsX = 0;
-            rowsY = 0;
+                if (mtc.Success)
+                {
+                    colsX = Int32.Parse(mtc.Groups[1].Value);
+                    rowsY = Int32.Parse(mtc.Groups[2].Value);
+
+                    int maxColorValue = Int32.Parse(allLines[firstLine++]);
+
+                    // Remember to skip the maximum color value byte
+                    allLines = allLines.Skip(firstLine).ToArray();
+
+                    // Now check the size line up correctly
+                    Console.WriteLine("Lines of data: {0}", allLines.Length);
+
+
+                    return true;
+                }
+
+            }
 
             return false;
         }
