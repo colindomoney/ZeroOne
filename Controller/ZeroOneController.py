4a2e94e010a4c341805e01c79baffd0fd4296aac
Colin Domoney <cdomoney@veracode.com>
2018-01-27 16:14:57 +0000

    Added the Python test files and controller project.

---
+++ Controller/ZeroOneController.py
@@ -0,0 +1,20 @@
+import opc, time
+
+numLEDs = 591
+client = opc.Client('localhost:7890')
+
+black = [ (0, 0, 0) ] * numLEDs
+white = [ (0, 255, 0) ] * numLEDs
+
+if client.can_connect():
+    print("Connected to server")
+else:
+    print("FAILED to connect to server")
+    exit(1)
+
+while True:
+    print('. ')
+    client.put_pixels(white)
+    time.sleep(0.25)
+    client.put_pixels(black)
+    time.sleep(0.25)
