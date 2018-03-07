49fb8297efe9038b9eae942295531beea3f318e5
Colin Domoney <cdomoney@veracode.com>
2018-03-07 13:48:25 +0000

    Tweaks to script to make it work with cron

--- RPI/check_wifi.sh
+++ RPI/check_wifi.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 ping -c4 192.168.9.4 > /dev/null
- 
-if [ $? != 0 ] 
+
+if [ $? != 0 ]
 then
-    echo "No network connection, rebooting at" $(date) 2>&1 | tee -a './check_wifi.log'
+    echo "No network connection, rebooting at" $(date) 2>&1 | tee -a '/home/pi/logs/check_wifi.log'
     sleep 2
     sudo reboot
 fi
